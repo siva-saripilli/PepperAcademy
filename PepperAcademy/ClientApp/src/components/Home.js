07545f587f675da756838ce4f08c7603b4c79287
ANKITA DHARNE <ankydharne@gmail.com>
2023-05-26 18:50:57 +1200

    added dropdown for course and level

--- PepperAcademy/ClientApp/src/components/Home.js
+++ PepperAcademy/ClientApp/src/components/Home.js
@@ -14,9 +14,21 @@ export class Home extends Component {
                     <label>Student</label><br></br>
                     <input></input><br></br>
                     <label>Course</label><br></br>
-                    <input></input><br></br>
+                    <select name="course" id="course">
+                        <option value="">Select Course</option>
+                        <option value="Maths">Maths</option>
+                        <option value="English">English</option>
+                        <option value="Physics">Physics</option>
+                    </select>
+                    <br></br>
                     <label>Level</label> <br></br>
-                    <input></input><br></br>
+                    <select name="level" id="level">
+                        <option value="">Select Level</option>
+                        <option value="Beginner">Beginner</option>
+                        <option value="Intermediate">Intermediate</option>
+                        <option value="Advanced">Advanced</option>
+                    </select>
+                    <br></br>
                     <label>Interest</label><br></br>
                     <input></input><br></br>
                 </form>
