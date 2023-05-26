6e532b0cbcbf08e8f5fcd5bf075023c232cf21dc
BenMWight <ben.m.wight@gmail.com>
2023-05-27 11:55:37 +1200

    Student Monitoring Added

--- PepperAcademy/ClientApp/src/components/Counter.js
+++ PepperAcademy/ClientApp/src/components/Counter.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import './Counter.css'
 export class Counter extends Component {
   static displayName = Counter.name;
 
@@ -15,17 +15,40 @@ export class Counter extends Component {
     });
   }
 
-  render() {
-    return (
-      <div>
-        <h1>Counter</h1>
+    render() {
+        const students = [
+            { "name": "John Doe", "subject": "Math", "level": "Beginner", "interest": "Star Wars", "age": 10, "questions_answered": 10, "questions_correct": 8, "questions_incorrect": 2, "percentage_correct": 80 },
+            { "name": "Jane Smith", "subject": "Science", "level": "Intermediate", "interest": "Cars", "age": 8, "questions_answered": 15, "questions_correct": 12, "questions_incorrect": 3, "percentage_correct": 80 },
+            { "name": "David Johnson", "subject": "English", "level": "Advanced", "interest": "Flowers", "age": 6, "questions_answered": 8, "questions_correct": 6, "questions_incorrect": 2, "percentage_correct": 75 },
+            { "name": "Sarah Thompson", "subject": "History", "level": "Beginner", "interest": "Space exploration", "age": 12, "questions_answered": 10, "questions_correct": 7, "questions_incorrect": 3, "percentage_correct": 70 },
+            { "name": "Michael Rodriguez", "subject": "Spanish", "level": "Intermediate", "interest": "Soccer", "age": 14, "questions_answered": 12, "questions_correct": 10, "questions_incorrect": 2, "percentage_correct": 83.3 },
+            { "name": "Emily Chen", "subject": "Biology", "level": "Intermediate", "interest": "Animals", "age": 16, "questions_answered": 18, "questions_correct": 15, "questions_incorrect": 3, "percentage_correct": 83.3 },
+            { "name": "Daniel Wilson", "subject": "Physics", "level": "Advanced", "interest": "Robotics", "age": 17, "questions_answered": 20, "questions_correct": 18, "questions_incorrect": 2, "percentage_correct": 90 },
+            { "name": "Sophia Lee", "subject": "Chemistry", "level": "Advanced", "interest": "Music", "age": 15, "questions_answered": 16, "questions_correct": 14, "questions_incorrect": 2, "percentage_correct": 87.5 },
+            { "name": "Olivia Brown", "subject": "Geography", "level": "Intermediate", "interest": "Travel", "age": 13, "questions_answered": 14, "questions_correct": 12, "questions_incorrect": 2, "percentage_correct": 85.7 },
+            { "name": "Thomas Wilson", "subject": "Computer Science", "level": "Advanced", "interest": "Coding", "age": 18, "questions_answered": 22, "questions_correct": 21, "questions_incorrect": 1, "percentage_correct": 95.5 }
+        ];
 
-        <p>This is a simple example of a React component.</p>
+        return (
 
-        <p aria-live="polite">Current count: <strong>{this.state.currentCount}</strong></p>
-
-        <button className="btn btn-primary" onClick={this.incrementCounter}>Increment</button>
-      </div>
+          <div>
+                <h1>Student Monitoring</h1>
+                <table className="student-table">
+                  <thead>
+                      <tr>
+                          <th>Name</th><th>Subject</th><th>Level</th><th>Interest</th><th>Age</th><th>Questions Answered</th><th>Questions Correct</th><th>Questions Incorrect</th><th>Percentage Correct</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      {students.map((student, index) => (
+                          <tr key={index}>
+                              <td>{student.name}</td><td>{student.subject}</td><td>{student.level}</td><td>{student.interest}</td><td>{student.age}</td><td>{student.questions_answered}</td><td>{student.questions_correct}</td><td>{student.questions_incorrect}</td><td>{student.percentage_correct}%</td>
+                          </tr>
+                      ))}
+                  </tbody>
+                </table>
+                <br />
+          </div>
     );
   }
 }
