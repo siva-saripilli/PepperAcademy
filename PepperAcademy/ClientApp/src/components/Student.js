d7ec93b27358103dd331a18775306eb8538e21a7
ANKITA DHARNE <ankydharne@gmail.com>
2023-05-27 13:41:41 +1200

    added quiz homepage

--- PepperAcademy/ClientApp/src/components/Student.js
+++ PepperAcademy/ClientApp/src/components/Student.js
@@ -1,7 +1,41 @@
 ﻿import React, { Component } from 'react';
 import Speech from './Speech';
+import './Student.css';
 
 export class Student extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            questions: [{id:"1", questions:""}],
+            answers: {},
+        };
+    }
+
+    componentDidMount() {
+        // Fetch questions from API
+        fetch('https://api.example.com/questions') // Replace with your API endpoint
+            .then((response) => response.json({}))
+            .then((data) => {
+                const answers = {};
+                data.forEach((question) => {
+                    answers[question.id] = '';
+                });
+                this.setState({ questions: data, answers: answers });
+            })
+            .catch((error) => {
+                console.error('Error fetching questions:', error);
+            });
+    }
+
+    handleAnswerChange = (questionId, answer) => {
+        this.setState((prevState) => ({
+            answers: {
+                ...prevState.answers,
+                [questionId]: answer,
+            },
+        }));
+    };
+
     render() {
         const urlParams = new URLSearchParams(window.location.search);
         const age = urlParams.get('age');
@@ -9,17 +43,42 @@ export class Student extends Component {
         const subject = urlParams.get('subject');
         const interest = urlParams.get('interest');
         const level = urlParams.get('level');
+        const { questions, answers } = this.state;
 
         return (
             <div className="main">
-                <div className="student-info">
-                    <h1>Student Information</h1>
+                <h1>Quiz Home Page</h1>
+                <div className="student-card ">
+                    <h3>Student Information</h3>
                     <p><strong>Name:</strong> {name}</p>
                     <p><strong>Subject:</strong> {subject}</p>
                     <p><strong>Interest:</strong> {interest}</p>
                     <p><strong>Level:</strong> {level}</p>
                     <p><strong>Age:</strong> {age}</p>
                 </div>
+                {questions.map((question) => (
+                    <div key={question.id} className="card" >
+                        <div className="card-header">Question {question.id}</div>
+                        <div className="card-body">
+                            <p className="card-question">{question.question}</p>
+                            <div className="answer">
+                                <input
+                                    type="text"
+                                    value={answers[question.id]}
+                                    onChange={(e) => this.handleAnswerChange(question.id, e.target.value)}
+                                    placeholder="Enter your answer"
+                                />
+                                 <div className="submitBtn">
+                                    <input type="submit" value="Submit" />
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+
+                ))};
                 <div className="voice-to-text">
                     <h1>Voice to Text</h1>
                     <Speech />
